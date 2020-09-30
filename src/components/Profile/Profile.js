51e6b145a7fea16ffd0905f73e6dcb8673688c1b
felipetempus <felipemouradev@gmail.com>
2020-09-30 04:52:12 -0400

    pause

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { Link, Route, Redirect } from 'react-router-dom';
-import { Button, Form, Input, FormGroup } from 'react-bootstrap';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 import API from "../../modules/data_module"
 
@@ -11,10 +16,12 @@ const Profile = (props) => {
     const getProfile = props_reference.getProfile
     
     return <>
-        <div className="container">
+        <Paper className="container">
+            <Grid>
             <h2>{profile.first_name} {profile.last_name}</h2>
             <h5>Joined in { profile.date_joined }</h5>
-        </div>
+            </Grid>
+        </Paper>
     </>
 }
 
